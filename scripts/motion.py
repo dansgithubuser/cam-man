822a051e5286e42c3301fd6a3b36ade09a59b989
Dan <daniel@foresightanalytics.ca>
2023-07-20 20:55:23 -0400

    scripts/motion.py width & height

--- scripts/motion.py
+++ scripts/motion.py
@@ -6,11 +6,17 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('camera_index', nargs='?', default=0, type=int)
+parser.add_argument('--width', type=int)
+parser.add_argument('--height', type=int)
 parser.add_argument('--background-smoothness', '-s', default=10.0, type=float)
 parser.add_argument('--diff-threshold', '--dt', default=0.05, type=float)
 args = parser.parse_args()
 
-cam = camman.Cam(args.camera_index)
+cam = camman.Cam(
+    args.camera_index,
+    args.width,
+    args.height,
+)
 motion_detector = camman.detector.Motion(args.background_smoothness, args.diff_threshold)
 window = camman.sink.Window(closeable=True)
 
